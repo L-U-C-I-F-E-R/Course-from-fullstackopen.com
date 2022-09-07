c8af2c33afe029590d4cc5d0295008406fde5efe
Maksym Kysil <44706766+L-U-C-I-F-E-R@users.noreply.github.com>
2022-09-07 12:40:53 +0300

    ex 1.6 - 1.11

--- course/src/App.js
+++ course/src/App.js
@@ -1,23 +1,76 @@
+
+// Exercises 1.6.-1.11.
 import { useState } from "react";
 
+const Statistick = ({allVoices, good, bad, neutral }) => {
+  let stat = good*100/allVoices
+  if (allVoices === 0){ 
+    return <div>
+      <p> No feedback given</p>
+    </div>
+    
+  }else {
+    return  <table>
+        {/* <tr>
+          <StatisticLine text="Good: "/>  нахрена я єто писал?)
+          <StatisticLine value ={good} />
+        </tr>
+        <tr>
+          <StatisticLine text="Neutral:"/>
+          <StatisticLine value ={neutral} />
+        </tr>
+        <tr>
+          <StatisticLine text="Bad:"/>
+          <StatisticLine value ={bad} />
+        </tr>
+        <tr>
+          <StatisticLine text="All voices:"/>
+          <StatisticLine value ={allVoices} />
+        </tr>
+        <tr>
+          <StatisticLine text="Average:"/>
+          <StatisticLine value ={(good - bad)/allVoices} />
+        </tr> 
+        <tr>
+          <StatisticLine text="Statistick:"/>
+          <StatisticLine value ={stat + ' %'} />
+        </tr> */}
+       
+      <StatisticLine text="Good:" value ={good} />
+      <StatisticLine text="Neutral:" value ={neutral} />
+      <StatisticLine text="Bad:" value ={bad} /> 
+      <StatisticLine text="All voices:" value ={allVoices} /> 
+      <StatisticLine text="Average:" value ={(good - bad)/allVoices} /> 
+      <StatisticLine text="Statistick:" value ={stat + ' %'} />  
+    </table>
+  }}
 
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
-  )
+  const StatisticLine = (props) => {
+    return <tr>
+        <td>{props.text}</td> 
+        <td>{props.value}</td>
+    </tr>
+  }
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allVoices, setAllVoices] = useState(0)
+  // const [average, setAver] = useState(0) 
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => {setGood(good + 1); setAllVoices(allVoices + 1)}}>good</button>
+      <button onClick={() => {setNeutral(neutral + 1); setAllVoices(allVoices + 1)}}>neutral</button>
+      <button onClick={() => {setBad(bad + 1); setAllVoices(allVoices + 1)}}>bad</button>
+      <h1>Statistick</h1>
+      <Statistick good = {good} allVoices = {allVoices} neutral={neutral} bad={bad} />
+    </div>
+  )
 }
-
-const App = ({ notes }) => {
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
-  )
-}
-
+ 
 export default App;
+
