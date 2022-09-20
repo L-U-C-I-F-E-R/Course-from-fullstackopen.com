8b5394c1b333caf8509066db2d4ed941383cf845
Maksym Kysil <44706766+L-U-C-I-F-E-R@users.noreply.github.com>
2022-09-20 19:04:26 +0300

    exper2

--- course/src/App.js
+++ course/src/App.js
@@ -1,47 +1,7 @@
-import { useState } from "react";
-import Note from "./components/Note";
 
-const App = (props) => {
+
+const App = () => {
   
-
-  const AddNote = (event) => {
-    event.preventDefault()
-    const newObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    }
-
-    setNotes(notes.concat(newObject))
-    setNewNote('')
-  }
-
-  const HandlerChange = (event) =>{
-    setNewNote(event.target.value)
-    console.log(event.target.value)
-  }
-
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'all' : "important"}
-        </button>
-        <form onSubmit={AddNote}>
-          <input 
-          onChange={HandlerChange}
-          value={newNote} />
-          <button type='submit'>save</button>
-        </form>
-      </ul>
-    </div>
-  )
 }
 
 export default App;
