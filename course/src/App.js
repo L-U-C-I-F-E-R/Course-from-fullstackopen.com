7d3777295235b57f0379dbefd0d6c3607f6d7b11
Maksym Kysil <44706766+L-U-C-I-F-E-R@users.noreply.github.com>
2022-09-20 18:56:38 +0300

    2 part - b 'forms'

--- course/src/App.js
+++ course/src/App.js
@@ -1,20 +1,49 @@
 import { useState } from "react";
+import Note from "./components/Note";
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('add...')
+  const [showAll, setShowAll] = useState(true)
 
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
-  )
-}
+  const AddNote = (event) => {
+    event.preventDefault()
+    const newObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
 
-const App = ({ notes }) => {
+    setNotes(notes.concat(newObject))
+    setNewNote('')
+  }
+
+  const HandlerChange = (event) =>{
+    setNewNote(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const notesToShow = showAll
+   ? notes 
+   : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'all' : "important"}
+        </button>
+        <form onSubmit={AddNote}>
+          <input 
+          onChange={HandlerChange}
+          value={newNote} />
+          <button type='submit'>save</button>
+        </form>
       </ul>
     </div>
   )
