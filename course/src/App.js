d90a7c470cf4b9b6c2f82d63ceab7f049ff98b00
Maksym Kysil <44706766+L-U-C-I-F-E-R@users.noreply.github.com>
2022-09-28 19:22:10 +0300

    2.8 step3 - adding number, comparing only names

--- course/src/App.js
+++ course/src/App.js
@@ -7,14 +7,16 @@ const App = () => {
 ])
 
     const [newPerson, setNewPerson] = useState('')
+    const [number, setNumber] = useState('')
 
     const AddName = (event) => {
         event.preventDefault()
         const newName = {
-            name: newPerson
+            name: newPerson,
+            number: number
         }
         //it`s working check on duplicate
-        const val = persons.map(person => JSON.stringify(person) === JSON.stringify(newName))
+        const val = persons.map(person => JSON.stringify(person.name) === JSON.stringify(newName.name))
         console.log(val) // ------ it`s [true, false]
         const falseOrTrue = val.some(element => element === true)
         console.log(falseOrTrue) // it`s just 'true' or 'false'
@@ -27,9 +29,13 @@ const App = () => {
     }
 
 
-    const HandlerChange = (event) => {
-        setNewPerson(event.target.value)
-        console.log(event.target.value)
+    const HandlerChange = (a, event) => {
+        console.log(a)
+        switch (a) {
+            case 1: setNewPerson(event.target.value);
+            break;
+            case 2: setNumber(event.target.value)
+        }
     }
 
 return(
@@ -37,8 +43,12 @@ return(
         <h1>Phonebook</h1>
         <form onSubmit={AddName}>
             <div>
-                name: <input onChange={HandlerChange}
+                Name: <input onChange={(e) => HandlerChange(1, e)}
                 value={newPerson}/>
+            </div>
+            <div>
+                Number: <input onChange={(e) => HandlerChange(2, e)} 
+                value={number}/>
             </div>
             <div>
                 <button type='submit'>Add</button>
@@ -46,8 +56,9 @@ return(
         </form>
         <h1>Numbers</h1>
         <ul>
-            {persons.map(person => <li key={person.name}>{person.name}</li>)}
-            {console.log(Object.getOwnPropertyNames(persons))}
+            {persons.map(person => <li key={person.name}>{person.name}: {person.number}</li>)}
+            {/* {console.log(persons[2].number)} */}
+            {/* {console.log(Object.getOwnPropertyNames(persons))} */}
         </ul>
     </div>    
 )
