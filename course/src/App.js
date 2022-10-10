be8fc08fb52ba4fec72788eac4b8074e5ca4c2f1
Maksym Kysil <44706766+L-U-C-I-F-E-R@users.noreply.github.com>
2022-10-10 21:36:25 +0300

    progres 1

--- course/src/App.js
+++ course/src/App.js
@@ -3,11 +3,51 @@ import axios from "axios"
 
 
 const App = () => {
+    const [input, setInput] = useState('')
+    const [countries, setCountries] = useState()
+
+    useEffect(() => {
+        axios
+        .get('https://restcountries.com/v3.1/all')
+        .then(responce => {
+            setCountries(responce.data)
+            console.log('OK')
+        })
+    },[])
     
+
+    const searchCountry = () => {
+        // console.log(countries[1].name.common)
+        const result = []
+        for(const data of countries){
+            // console.log(country.name.common)
+            const country = data.name.common.JSON.stringfy()
+            console.log(country)
+            // if (country.includes(input) || country.toLowwerCase().includes(input)){
+            //     result.concat(country)
+            // }
+        }
+        
+    }
+
+    const showCountry = () => {
+
+    }
+
+    const HandlerEvent = (event) => {
+        // event.preventDefault()
+        setInput(event.target.value)
+        // console.log(input)
+    }
 
 return(
     <div>
-        
+        <form onChange = {searchCountry}>
+            <p>Find countries</p>
+            <input 
+                onChange={HandlerEvent}
+                value={input}/>
+        </form>
     </div>    
 )
 }
